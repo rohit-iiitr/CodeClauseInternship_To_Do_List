91e3ccf3792ac06a1a8aff428a97e796c37a6bf0
Rohit_Patel_IIITR <127647959+rohit-iiitr@users.noreply.github.com>
2023-12-25 18:44:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const inputBox=document.getElementById("input-box");
+const listcontainer=document.getElementById("list-container");
+
+function addtask(){
+    if(inputBox.value==='') alert("You must write something");
+    else{
+        let li= document.createElement("li");
+        li.innerHTML= inputBox.value;
+        listcontainer.appendChild(li);
+    }
+    inputBox.value="";
+}
